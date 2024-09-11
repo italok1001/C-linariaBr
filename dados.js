ba5cf5dd84c6018335dbc0338d42bdfa184a9376
italok1001 <65143798+italok1001@users.noreply.github.com>
2024-09-11 15:50:09 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,55 @@
+let dados = [
+   {
+     titulo: "Feijoada",
+    descrição: "A feijoada, um prato nacional brasileiro, é uma verdadeira ode ao sabor e à tradição. Essa iguaria, rica em história e cultura, conquista paladares por todo o país com sua combinação única de ingredientes e aromas.",          
+     link: "https://www.youtube.com/watch?v=2SpzVuUlwDg",
+     tags: "feijão feijao feijoada feijao preto"
+    },
+    {
+    titulo: "Arroz",
+     descrição: "O arroz é um alimento básico em muitas culturas, conhecido por sua versatilidade e sabor neutro.",
+     link: "https://www.youtube.com/watch?v=IhUKu07aFkM",
+    tags: "arroz"
+     },
+    {
+    titulo: "Macarrão à Bolonhesa",
+     descrição: "O macarrão à bolonhesa é um clássico da culinária italiana, feito com um molho à base de carne moída, tomate e especiarias.",
+     link: "https://www.youtube.com/watch?v=-qfDCzR0bA0",
+     tags:"macarrao Macarrao macarrão",
+     },
+     {
+      titulo: "Moqueca Capixaba",
+      descrição: "A moqueca capixaba é um prato à base de peixe, camarão e palmito, cozido em um caldo de leite de coco e temperos frescos. Uma explosão de sabores do litoral!",
+      link: "https://www.youtube.com/watch?v=LO9s4UIFTrA",
+      tags: "moqueca peixe camarão palmito"
+  },
+  {
+      titulo: "Vatapá",
+      descrição: "O vatapá é um prato típico da Bahia, feito com pão amanhecido, camarão seco, azeite de dendê e uma variedade de temperos. Um sabor marcante e único!",
+      link: "https://www.youtube.com/watch?v=ZM93zAp7bkk",
+      tags: "vatapá bahia camarão azeite de dendê"
+  },
+  {
+      titulo: "Bobó de Camarão",
+      descrição: "O bobó de camarão é outro clássico da culinária baiana, feito com camarão, mandioca, leite de coco e azeite de dendê. Um prato cremoso e saboroso!",
+      link: "https://www.youtube.com/watch?v=UITsK_S3I_Y",
+      tags: "bobó de camarão bahia camarão mandioca"
+  },
+  {
+      titulo: "Pão de Queijo",
+      descrição: "O pão de queijo é um dos salgadinhos mais populares do Brasil, feito com polvilho doce, queijo e ovo. Uma delícia para qualquer hora do dia!",
+      link: "https://www.youtube.com/watch?v=FDLH4s4qktU",
+      tags: "pão de queijo polvilho queijo"
+  },
+  {
+      titulo: "Coxinha",
+      descrição: "A coxinha é outro salgado brasileiro muito popular, feita com massa de batata e recheada com frango desfiado. Irresistível!",
+      link: "https://www.youtube.com/watch?v=pyslNp5YCj8",
+      tags: "coxinha frango batata salgado"
+  }
+    ];
+
+
+
+
+
